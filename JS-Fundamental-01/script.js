d471c9f3640c86d4a77720a7cacdbb8f66590603
NH1Tkendo <ngobatai1711@gmail.com>
2025-07-04 19:34:52 +0700

    Day 1: 0 - 20 done
    Signed-off-by: NH1Tkendo <ngobatai1711@gmail.com>

---
+++ JS-Fundamental-01/script.js
@@ -0,0 +1,47 @@
+let language = 'Vietnamese';
+const country = 'VietNam';
+const continent = 'Asia';
+const isIsland = false;
+
+//Basic operator
+// let population = 100000000;
+// let finPopulation = 6000000;
+// const avePop = 33000000;
+
+// console.log(population / 2);
+// console.log(population + 1);
+// console.log(population > finPopulation);
+// console.log(population < avePop);
+//========================================
+
+//If/else statements
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(
+//     `${country}'s population is ${33 - population} million
+//     below average`
+//   );
+// }
+//==================================
+
+// Challenge 1
+// const massMark = 78, heightMark = 1.69, massJohn = 92, heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn);
+// console.log(markHigherBMI);
+
+//Strings and template literals
+//const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+//==================================
+
+//Challenge 2
+// if (BMIMark > BMIJohn) {
+//     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+// } else {
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+// }
